0b5072b836b7c232542f7dc5591ddad059fc6335
Alessandro Frigeri <alessandro.frigeri@gmail.com>
2011-08-31 03:15:41 +0200

    Initial commit

---
+++ src/alf/gasdroide/GasdroideActivity.java
@@ -0,0 +1,13 @@
+package alf.gasdroide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GasdroideActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
